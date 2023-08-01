94761ce2a3a52c5da9f082e6b7c92060959862d2
guilealp <guilherme.3110@hotmail.com>
2023-08-01 10:09:52 -0300

    exercicio 8

--- app/Http/Controllers/ExercicioOitoController.php
+++ app/Http/Controllers/ExercicioOitoController.php
@@ -6,5 +6,16 @@ use Illuminate\Http\Request;
 
 class ExercicioOitoController extends Controller
 {
-    //
+    public function exibirtabuada(Request $request)
+    {
+        $numeros = []; 
+
+        for ($i = 1; $i <= 10; $i++) {
+          array_push($numeros, $i * $request->numero);
+        }
+
+        return json_encode([
+            'valores'=> $numeros
+        ]);
+    }
 }
